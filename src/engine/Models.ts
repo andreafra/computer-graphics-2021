4b71b9e63fb07c6863ba52960fde402dc534d969
Alessandro Astone <ales.astone@gmail.com>
2021-08-29 15:34:38 +0200

    Clamp UV coordinates

--- src/engine/Models.ts
+++ src/engine/Models.ts
@@ -55,6 +55,8 @@ export function MakeTexture(
 			gl.UNSIGNED_BYTE,
 			textureImage
 		);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 		gl.texParameteri(
 			gl.TEXTURE_2D,
